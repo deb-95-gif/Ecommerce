6d49fb5ea405eba747681a2d2e4ca562e885f59f
Debanjan Bhowmick <bhowmick.debanjan@gmail.com>
2021-08-26 12:38:10 +0530

    SecondCommiT

--- src/main/java/com/qa/ecommerece/pages/OrderPage.java
+++ src/main/java/com/qa/ecommerece/pages/OrderPage.java
@@ -8,6 +8,8 @@ import com.qa.ecommerce.base.TestBase;
 
 public class OrderPage extends TestBase {
 	
+	HomePage home;
+	
 	//Web Element Locators
 	@FindBy(xpath = "//td[@class='cart_product']//img[@alt='Faded Short Sleeve T-shirts']")
 	WebElement prdImg;
@@ -21,8 +23,31 @@ public class OrderPage extends TestBase {
 	@FindBy(xpath = "//span[@class='heading-counter']")
 	WebElement msg;
 	
-	@FindBy(xpath = "//td[@class='price']//span[contains(text(),'$18.51')]")
-
+	@FindBy(xpath = "//tr[7]//td[2]")
+	WebElement total;
+	
+	@FindBy(xpath = "//td[@class='cart_avail']")
+	WebElement stockavilbility;
+	
+	@FindBy(xpath = "//tbody/tr[@class='cart_item last_item first_item address_0 odd']/td[@class='cart_unit']/span[1]")
+	WebElement unitPrice;
+	
+	@FindBy(xpath = "//td[contains(text(),' ')]")
+	WebElement prdTotal;
+	
+	@FindBy(xpath = "//td[normalize-space()=' ']")
+	WebElement totalTax;
+	
+	@FindBy(xpath = "//h1[@class='page-heading']")
+	WebElement heading;
+	
+	@FindBy(xpath = "//span[contains(text(),'DEBANJAN BHOWMICK')]")
+	WebElement cstName;
+	
+	@FindBy(css = ".button.btn.btn-default.standard-checkout.button-medium")
+	WebElement prcCheckBtn;
+	
+	
 	public OrderPage() {
 		PageFactory.initElements(driver, this);
 	}
@@ -43,6 +68,43 @@ public class OrderPage extends TestBase {
 		minusQty.click();
 		String message = msg.getText();
 		return message;
+	}
+	
+	public String verifyTotal() {
+		String value = total.getAttribute(" ");
+		return value;
+	}
+	
+	public boolean verifyStockavailbility() {
+		boolean flag = false;
+		flag = stockavilbility.isDisplayed();
+		return flag;
+	}
+	
+	public String Total() {
+		String prd = prdTotal.getAttribute(" ");
+		String tax = totalTax.getAttribute(" ");
+		return prd+tax;
+	}
+	
+	public String verifyUnitPrice() {
+		String price  = unitPrice.getAttribute(" ");
+		return price;
+	}
+	
+	public String verifyPageHeading() {
+		String pageHeading = heading.getText();
+		return pageHeading;
+	}
+	
+	public void clickOnCheckOut() {
+		prcCheckBtn.click();
+		if(cstName.getText().matches(" ")) {
+			home.login(prop.getProperty("username"), prop.getProperty("password"));
+		}
+		else (cstName.getText().matches("DEBANJAN")){
+			
+		}
 	}
 
 }
--- src/test/java/com/qa/ecommerce/tests/OrderPageTest.java
+++ src/test/java/com/qa/ecommerce/tests/OrderPageTest.java
@@ -1,5 +1,7 @@
 package com.qa.ecommerce.tests;
 
+import static org.testng.Assert.assertEquals;
+
 import java.io.IOException;
 
 import org.testng.Assert;
@@ -35,16 +37,47 @@ public class OrderPageTest extends TestBase {
 //		Assert.assertEquals(title, "Order - My Store" );
 //	}
 //	
-	@Test(priority = 2)
-	public void validateChangeinQuantityMessage() {
-		String message = order.verifyChangeinQtyMessageadd();
-		Assert.assertEquals(message, order.verifyChangeinQtyMessageadd());
-	}
+//	@Test(priority = 2)
+//	public void validateChangeinQuantityMessage() {
+//		String message = order.verifyChangeinQtyMessageadd();
+//		Assert.assertEquals(message, order.verifyChangeinQtyMessageadd());
+//	}
+//	
+//	@Test(priority = 3)
+//	public void validateChangeinQuantityMessagesb() {
+//		String message = order.verifyChangeinQtyMessageasub();
+//		Assert.assertEquals(message, order.verifyChangeinQtyMessageasub());
+//	}
+//	
+//	@Test(priority = 4)
+//	public void validateTotalChange() {
+//		String total  = order.verifyTotal();
+//		Assert.assertEquals(order.verifyTotal(), total);
+//	}
 	
-	@Test(priority = 3)
-	public void validateChangeinQuantityMessagesb() {
-		String message = order.verifyChangeinQtyMessageasub();
-		Assert.assertEquals(message, order.verifyChangeinQtyMessageasub());
+//	@Test(priority = 5)
+//	public void validateStock() {
+//		boolean stock = false;
+//		stock = order.verifyStockavailbility();
+//		Assert.assertEquals(stock, true);
+//	}
+	
+//	@Test(priority = 6)
+//	public void validateTotal() {
+//		String total = order.verifyTotal();
+//		Assert.assertEquals(total, order.verifyTotal());
+//	}
+	
+//	@Test(priority = 7)
+//	public void validateUnitPrice() {
+//		String untPrc = order.verifyUnitPrice();
+//		Assert.assertEquals(untPrc, order.verifyUnitPrice());
+//	}
+	
+	@Test(priority = 8)
+	public void validatePageHeading() {
+		String heading = order.verifyPageHeading();
+		Assert.assertEquals(heading, order.verifyPageHeading());
 	}
 	
 	@AfterMethod
